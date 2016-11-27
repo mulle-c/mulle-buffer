4b79ad3fafdbf15db239bb8e5044e91d571a3dbd
Nat! <nat@mulle-kybernetik.com>
2016-11-27 22:07:21 +0100

    add release mechanics

---
+++ bin/release.sh
@@ -0,0 +1,66 @@
+#! /bin/sh
+
+PROJECT="MulleBuffer" # requires camel-case
+DESC="A growable C array of unsigned chars"
+DEPENDENCIES='${DEPENDENCY_TAP}/mulle-c11
+${DEPENDENCY_TAP}/mulle-allocator'  # no camel case, will be evaled later!
+LANGUAGE=c               # c,cpp, objc
+
+#
+# Ideally you don't hafta change anything below this line
+#
+# source mulle-homebrew.sh (clumsily)
+MULLE_BOOTSTRAP_FAIL_PREFIX="release.sh"
+
+. ./bin/repository-info.sh || exit 1
+. ./bin/mulle-homebrew/mulle-homebrew.sh || exit 1
+
+
+# parse options
+homebrew_parse_options "$@"
+
+# dial past options
+while [ $# -ne 0 ]
+do
+   case "$1" in
+      -*)
+         shift
+      ;;
+      *)
+         break;
+      ;;
+   esac
+done
+
+
+#
+# these can usually be deduced, if you follow the conventions
+#
+NAME="`get_name_from_project "${PROJECT}" "${LANGUAGE}"`"
+HEADER="`get_header_from_name "${NAME}"`"
+VERSIONNAME="`get_versionname_from_project "${PROJECT}"`"
+VERSION="`get_header_version "${HEADER}" "${VERSIONNAME}"`"
+
+HOMEPAGE="`eval echo "${HOMEPAGE}"`"
+
+
+# --- HOMEBREW TAP ---
+# Specify to where and under what bame to publish via your brew tap
+#
+RBFILE="${NAME}.rb"                    # ruby file for brew
+HOMEBREWTAP="../homebrew-software"     # your tap repository path
+
+
+# --- GIT ---
+# tag to tag your release
+# and the origin where
+TAG="${1:-${TAGPREFIX}${VERSION}}"
+
+
+main()
+{
+   git_main "${ORIGIN}" "${TAG}" || exit 1
+   homebrew_main
+}
+
+main "$@"
