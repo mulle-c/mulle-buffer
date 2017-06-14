969546b3f918a51c1c096988ce6c6b5ad06aaeff
Nat! <nat@mulle-kybernetik.com>
2017-06-14 15:37:44 +0200

    Use templates to create .md files where necessary

--- bin/formula-info.sh
+++ bin/formula-info.sh
@@ -1,4 +1,4 @@
-PROJECT="MulleBuffer"   # your project name, requires camel-case
+PROJECT="mulle-buffer"   # your project name, requires camel-case
 DESC="A growable C array of unsigned chars"
 DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11
 ${DEPENDENCY_TAP}mulle-allocator'
