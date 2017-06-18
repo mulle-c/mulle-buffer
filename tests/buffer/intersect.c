a55b6c127f2178c48b096e30ee0649ecf3b9fce6
Nat! <nat@mulle-kybernetik.com>
2017-06-18 13:01:18 +0200

    fix wrong return code

--- tests/buffer/intersect.c
+++ tests/buffer/intersect.c
@@ -9,14 +9,16 @@ int  main()
    buffer._storage  = (void *) 0x10;
    buffer._sentinel = (void *) 0x20;
 
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x0, 0x10));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x0, 0x10));
 
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x0,  0x11));
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x10, 0x10));
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x10, 0x11));
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x1F, 0x10));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x0,  0x11));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x10, 0x10));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x10, 0x11));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x1F, 0x10));
 
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x20, 0x10));
-    printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x11, 0x0));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x20, 0x10));
+   printf( "%d\n", mulle_buffer_intersects_bytes( &buffer, (void *) 0x11, 0x0));
+
+   return( 0);
 }
 
