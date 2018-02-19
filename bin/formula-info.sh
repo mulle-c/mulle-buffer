875d17021f8742ab345cec7b218bd388f9542d89
Nat! <nat@mulle-kybernetik.com>
2018-02-19 23:49:51 +0100

    remove .bootstrap, remove bin, add mulle-project, remove templates

--- bin/formula-info.sh
+++
@@ -1,4 +0,0 @@
-PROJECT="mulle-buffer"   # your project name, requires camel-case
-DESC="A growable C array of unsigned chars"
-DEPENDENCIES='${DEPENDENCY_TAP}mulle-c11
-${DEPENDENCY_TAP}mulle-allocator'
