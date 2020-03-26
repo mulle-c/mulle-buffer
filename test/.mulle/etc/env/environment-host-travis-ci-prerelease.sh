fdb77914a01a988c43b68efd370152ebdb3c5316
Nat! <nat@codeon.de>
2020-03-26 15:04:19 +0100

    add missing file for travis

---
+++ test/.mulle/etc/env/environment-host-travis-ci-prerelease.sh
@@ -0,0 +1,163 @@
+# Fill it with mulle-env envirionment --scope  set <key> <value>
+#
+# mulle-testallocator nodetype: ${MULLE_TESTALLOCATOR_NODETYPE:-tar}
+#
+export MULLE_TESTALLOCATOR_NODETYPE="git"
+
+
+#
+# mulle-testallocator url: ${MULLE_TESTALLOCATOR_URL:-https://github.com/mulle-core/mulle-testallocator/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_TESTALLOCATOR_URL="https://github.com/mulle-core/mulle-testallocator.git"
+
+
+#
+# mulle-testallocator branch: ${MULLE_TESTALLOCATOR_BRANCH}
+#
+export MULLE_TESTALLOCATOR_BRANCH="prerelease"
+
+
+#
+# mulle-thread nodetype: ${MULLE_THREAD_NODETYPE:-tar}
+#
+export MULLE_THREAD_NODETYPE="git"
+
+
+#
+# mulle-thread url: ${MULLE_THREAD_URL:-https://github.com/mulle-concurrent/mulle-thread/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_THREAD_URL="https://github.com/mulle-concurrent/mulle-thread.git"
+
+
+#
+# mulle-thread branch: ${MULLE_THREAD_BRANCH}
+#
+export MULLE_THREAD_BRANCH="prerelease"
+
+
+#
+# mulle-c11 nodetype: ${MULLE_C11_NODETYPE:-tar}
+#
+export MULLE_C11_NODETYPE="git"
+
+
+#
+# mulle-c11 url: ${MULLE_C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
+
+
+#
+# mulle-c11 branch: ${MULLE_C11_BRANCH}
+#
+export MULLE_C11_BRANCH="prerelease"
+
+
+#
+# mulle-allocator nodetype: ${MULLE_ALLOCATOR_NODETYPE:-tar}
+#
+export MULLE_ALLOCATOR_NODETYPE="git"
+
+
+#
+# mulle-allocator url: ${MULLE_ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
+
+
+#
+# mulle-allocator branch: ${MULLE_ALLOCATOR_BRANCH}
+#
+export MULLE_ALLOCATOR_BRANCH="prerelease"
+
+
+#
+# mulle-stacktrace nodetype: ${MULLE_STACKTRACE_NODETYPE:-tar}
+#
+export MULLE_STACKTRACE_NODETYPE="git"
+
+
+#
+# mulle-stacktrace url: ${MULLE_STACKTRACE_URL:-https://github.com/mulle-core/mulle-stacktrace/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_STACKTRACE_URL="https://github.com/mulle-core/mulle-stacktrace.git"
+
+
+#
+# mulle-stacktrace branch: ${MULLE_STACKTRACE_BRANCH}
+#
+export MULLE_STACKTRACE_BRANCH="prerelease"
+
+
+#
+# mulle-dlfcn nodetype: ${MULLE_DLFCN_NODETYPE:-tar}
+#
+export MULLE_DLFCN_NODETYPE="git"
+
+
+#
+# mulle-dlfcn url: ${MULLE_DLFCN_URL:-https://github.com/mulle-core/mulle-dlfcn/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_DLFCN_URL="https://github.com/mulle-core/mulle-dlfcn.git"
+
+
+#
+# mulle-dlfcn branch: ${MULLE_DLFCN_BRANCH}
+#
+export MULLE_DLFCN_BRANCH="prerelease"
+
+
+#
+# mulle-atinit nodetype: ${MULLE_ATINIT_NODETYPE:-tar}
+#
+export MULLE_ATINIT_NODETYPE="git"
+
+
+#
+# mulle-atinit url: ${MULLE_ATINIT_URL:-https://github.com/mulle-core/mulle-atinit/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_ATINIT_URL="https://github.com/mulle-core/mulle-atinit.git"
+
+
+#
+# mulle-atinit branch: ${MULLE_ATINIT_BRANCH}
+#
+export MULLE_ATINIT_BRANCH="prerelease"
+
+
+#
+# mulle-atexit nodetype: ${MULLE_ATEXIT_NODETYPE:-tar}
+#
+export MULLE_ATEXIT_NODETYPE="git"
+
+
+#
+# mulle-atexit url: ${MULLE_ATEXIT_URL:-https://github.com/mulle-core/mulle-atexit/archive/${MULLE_TAG}.tar.gz}
+#
+export MULLE_ATEXIT_URL="https://github.com/mulle-core/mulle-atexit.git"
+
+
+#
+# mulle-atexit branch: ${MULLE_ATEXIT_BRANCH}
+#
+export MULLE_ATEXIT_BRANCH="prerelease"
+
+
+#
+# mulle-buffer nodetype: ${MULLE_BUFFER_NODETYPE:-tar}
+#
+export MULLE_BUFFER_NODETYPE="git"
+
+
+#
+# mulle-buffer url: ${MULLE_BUFFER_URL:-https://github.com/mulle-c/mulle-buffer/archive/${NODE_TAG}.tar.gz}
+#
+export MULLE_BUFFER_URL="https://github.com/mulle-c/mulle-buffer.git"
+
+
+#
+# mulle-buffer branch: ${MULLE_BUFFER_BRANCH}
+#
+export MULLE_BUFFER_BRANCH="prerelease"
+
+
