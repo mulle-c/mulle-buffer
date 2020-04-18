56ac3814506871b928895194dabd8c16ed1c8ced
Nat! <nat@mulle-kybernetik.com>
2020-04-18 19:00:59 +0200

    * new mulle-sde project structure

--- src/mulle-buffer.h
+++ src/mulle-buffer.h
@@ -38,7 +38,7 @@
 #ifndef mulle_buffer__h__
 #define mulle_buffer__h__
 
-#define MULLE_BUFFER_VERSION  ((2 << 20) | (1 << 8) | 0)
+#define MULLE_BUFFER_VERSION  ((2 << 20) | (1 << 8) | 1)
 
 #include "include.h"
 #include "mulle--buffer.h"
