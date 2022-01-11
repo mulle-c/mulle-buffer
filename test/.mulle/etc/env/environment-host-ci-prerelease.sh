2f67a0a554221d9d6c0a7cdcab3795feb8ddfe8a
Nat! <nat@mulle-kybernetik.com>
2022-01-11 02:30:00 +0100

    prerelease

--- test/.mulle/etc/env/environment-host-ci-prerelease.sh
+++ test/.mulle/etc/env/environment-host-ci-prerelease.sh
@@ -107,6 +107,24 @@ export MULLE_DLFCN_BRANCH="prerelease"
 
 
 #
+# dlfcn-win32 nodetype: ${DLFCN_WIN32_NODETYPE:-tar}
+#
+export DLFCN_WIN32_NODETYPE="git"
+
+
+#
+# dlfcn-win32 url: ${DLFCN_WIN32_URL:-https://github.com/mulle-core/dlfcn-win32/archive/refs/tags/${MULLE_TAG}.tar.gz}
+#
+export DLFCN_WIN32_URL="https://github.com/mulle-core/dlfcn-win32.git"
+
+
+#
+# dlfcn-win32 branch: ${DLFCN_WIN32_BRANCH}
+#
+export DLFCN_WIN32_BRANCH="prerelease"
+
+
+#
 # mulle-atinit nodetype: ${MULLE_ATINIT_NODETYPE:-tar}
 #
 export MULLE_ATINIT_NODETYPE="git"
@@ -149,7 +167,7 @@ export MULLE_BUFFER_NODETYPE="git"
 
 
 #
-# mulle-buffer url: ${MULLE_BUFFER_URL:-https://github.com/mulle-c/mulle-buffer/archive/${NODE_TAG}.tar.gz}
+# mulle-buffer url: ${MULLE_BUFFER_URL:-https://github.com/mulle-c/mulle-buffer/archive/${MULLE_TAG}.tar.gz}
 #
 export MULLE_BUFFER_URL="https://github.com/mulle-c/mulle-buffer.git"
 
