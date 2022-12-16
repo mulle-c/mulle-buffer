147ff6346b08e5f6fec9fa681a21a1c2df735463
Nat! <nat@mulle-kybernetik.com>
2022-12-16 15:15:58 +0100

    fix CI settings

--- .mulle/etc/env/environment-host-ci-prerelease.sh
+++ .mulle/etc/env/environment-host-ci-prerelease.sh
@@ -1,54 +1,54 @@
 #
-# mulle-allocator nodetype: ${MULLE_ALLOCATOR_NODETYPE:-tar}
+# mulle-allocator nodetype: ${MULLE__ALLOCATOR_NODETYPE:-tar}
 #
-export MULLE_ALLOCATOR_NODETYPE="git"
+export MULLE__ALLOCATOR_NODETYPE="git"
 
 
 #
-# mulle-allocator url: ${MULLE_ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
+# mulle-allocator url: ${MULLE__ALLOCATOR_URL:-https://github.com/mulle-c/mulle-allocator/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE_ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
+export MULLE__ALLOCATOR_URL="https://github.com/mulle-c/mulle-allocator.git"
 
 
 #
-# mulle-allocator branch: ${MULLE_ALLOCATOR_BRANCH}
+# mulle-allocator branch: ${MULLE__ALLOCATOR_BRANCH}
 #
-export MULLE_ALLOCATOR_BRANCH="prerelease"
+export MULLE__ALLOCATOR_BRANCH="prerelease"
 
 
 #
-# mulle-c11 nodetype: ${MULLE_C11_NODETYPE:-tar}
+# mulle-c11 nodetype: ${MULLE__C11_NODETYPE:-tar}
 #
-export MULLE_C11_NODETYPE="git"
+export MULLE__C11_NODETYPE="git"
 
 
 #
-# mulle-c11 url: ${MULLE_C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
+# mulle-c11 url: ${MULLE__C11_URL:-https://github.com/mulle-c/mulle-c11/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE_C11_URL="https://github.com/mulle-c/mulle-c11.git"
+export MULLE__C11_URL="https://github.com/mulle-c/mulle-c11.git"
 
 
 #
-# mulle-c11 branch: ${MULLE_C11_BRANCH}
+# mulle-c11 branch: ${MULLE__C11_BRANCH}
 #
-export MULLE_C11_BRANCH="prerelease"
+export MULLE__C11_BRANCH="prerelease"
 
 
 #
-# mulle-data nodetype: ${MULLE_DATA_NODETYPE:-tar}
+# mulle-data nodetype: ${MULLE__DATA_NODETYPE:-tar}
 #
-export MULLE_DATA_NODETYPE="git"
+export MULLE__DATA_NODETYPE="git"
 
 
 #
-# mulle-data url: ${MULLE_DATA_URL:-https://github.com/mulle-c/mulle-data/archive/${MULLE_TAG}.tar.gz}
+# mulle-data url: ${MULLE__DATA_URL:-https://github.com/mulle-c/mulle-data/archive/${MULLE_TAG}.tar.gz}
 #
-export MULLE_DATA_URL="https://github.com/mulle-c/mulle-data.git"
+export MULLE__DATA_URL="https://github.com/mulle-c/mulle-data.git"
 
 
 #
-# mulle-data branch: ${MULLE_DATA_BRANCH}
+# mulle-data branch: ${MULLE__DATA_BRANCH}
 #
-export MULLE_DATA_BRANCH="prerelease"
+export MULLE__DATA_BRANCH="prerelease"
 
 
